41297d15badbdab22b6f3c325c1ec3f7d0af1e5d
menareus <cameron.j.temple@gmail.com>
2023-09-30 21:16:23 -0400

    +Heal function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 const DEFAULT_ATTACK = 10;
 const DEFAULT_STRONG_ATTACK = 20
 const DEFAULT_MONSTER_ATTACK = 10
-
+const DEFAULT_HEAL = 10
 
 let DEFAULT_HEALTH = 100;
 
@@ -13,33 +13,39 @@ adjustHealthBars(DEFAULT_HEALTH);
 
 function turn(mode){
 let maxDamage
-if (mode === 'ATTACK') {
-    maxDamage = DEFAULT_ATTACK 
-}else if (mode === 'STRONG ATTACK'){
-    maxDamage = DEFAULT_STRONG_ATTACK
-}
-    const damage = dealMonsterDamage(maxDamage);
+    if (mode === 'ATTACK') {
+        maxDamage = DEFAULT_ATTACK 
+    } else if (mode === 'STRONG ATTACK'){
+        maxDamage = DEFAULT_STRONG_ATTACK
+    } 
+const damage = dealMonsterDamage(maxDamage);
     $monster_Health -= damage;
+}
+
+function endRound(){
     const hurt = dealPlayerDamage(DEFAULT_MONSTER_ATTACK);
     $player_Health -= hurt;
+    if ($monster_Health  >= 0 && $player_Health <= 0) {
+        alert('you lose')
+    } else if ($player_Health > 0 && $monster_Health <= 0){
+        alert('you wun!')
+    } else if ($player_Health <= 0 && $monster_Health <= 0){
+        alert('you both died. Tragic.')
+    }   
 }
-// REPLACE WITH FUNCTION Check(){}
-if ($monster_Health  >= 0 && $player_Health <= 0) {
-    alert('you lose')
-} else if ($player_Health > 0 && $monster_Health <= 0){
-    alert('you wun!')
-} else if ($player_Health <= 0 && $monster_Health <= 0){
-    alert('you both died. Tragic.')
-}
-    
+//BUTTON HANDLERS
 function attack (){
 turn('ATTACK');
-    }
-
+endRound()
+}
 function strongAttack(){
  turn('STRONG ATTACK');
-    }
-        
+ endRound()
+}
+function heal(){
+increasePlayerHealth(DEFAULT_HEAL);
+}        
+
 attackBtn.addEventListener('click', attack);
 strongAttackBtn.addEventListener('click', strongAttack);
-
+healBtn.addEventListener('click', heal);
